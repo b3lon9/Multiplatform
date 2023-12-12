304cd5629d065318ae2450957977ab984d19df3a
neander <b3xlon9@gmail.com>
2023-12-12 17:58:36 +0900

    hybrid - webview를 띄우고 backpressed 뒤로가기 구현 완료

--- Hybrid/lib/widgets/home_widget.dart
+++ Hybrid/lib/widgets/home_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hybrid/webview/webview_controller.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -10,6 +11,8 @@ class HomeWidget extends StatefulWidget {
 }
 
 class _HomeWidgetState extends State<HomeWidget> {
+  bool isPageLoadFinished = false;
+
   var webviewController = WebViewController()
     ..setJavaScriptMode(JavaScriptMode.unrestricted)
     ..setBackgroundColor(const Color(0x00000000))
@@ -18,9 +21,16 @@ class _HomeWidgetState extends State<HomeWidget> {
         onPageStarted: (url) => print('onPageStated.. url:$url'),
         onPageFinished: (url) => print('onPageFinished.. url:$url'),
         onProgress: (progress) => print('onProgress.. progress:$progress'),
+        onNavigationRequest: (request) {
+          print('onNavigationRequest.. $request');
+
+          // 다른 페이지로 이동하지 않게 막음
+          return NavigationDecision.navigate;
+        },
       )
     )
     ..loadRequest(Uri.parse("https://m.ssamplus.com/index_songcheck.asp"));
+
 
   @override
   void initState() {
@@ -29,10 +39,24 @@ class _HomeWidgetState extends State<HomeWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: WebViewWidget(
-          controller: webviewController,
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (bool didPop) async {
+        if (await webviewController.canGoBack()) {
+          webviewController.goBack();
+        } else {
+          SystemNavigator.pop(animated: true);
+        }
+      },
+      child: Scaffold(
+        body: Stack(
+          children: [
+            WebViewWidget(
+              controller: webviewController,
+            ),
+          ],
         ),
+      ),
     );
   }
 }
