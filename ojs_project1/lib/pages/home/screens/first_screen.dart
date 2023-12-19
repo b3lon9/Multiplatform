31e185c1b1ec983c2a976d4e66658c2698972d3a
neander <b3xlon9@gmail.com>
2023-12-19 11:20:48 +0900

    ojs chapter01 - carousel slider 적용하여 자동으로 이미지가 움직이도록 설정

--- ojs_project1/lib/pages/home/screens/first_screen.dart
+++ ojs_project1/lib/pages/home/screens/first_screen.dart
@@ -1,129 +1,164 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class FirstScreen extends StatelessWidget {
   const FirstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Column(
+          children: <Widget>[
+            _buildTop(),
+            _buildMiddle(),
+            _buildBottom(),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _buildTop() {
     return Padding(
       padding: const EdgeInsets.only(top: 20, bottom: 20),
       child: Column(
-        children: <Widget>[
-          _buildTop(),
-          _buildMiddle(),
-          _buildBottom(),
+        children: [
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('taxi'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('taxi'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('taxi'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('taxi'),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('taxi'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('taxi'),
+                ],
+              ),
+              InkWell(
+                onTap: () {
+                  print('last taxi click..');
+                },
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('taxi'),
+                  ],
+                ),
+              ),
+              Opacity(
+                opacity: 0.0,
+                child: Column(
+                  children: <Widget>[
+                    Icon(
+                      Icons.local_taxi,
+                      size: 40,
+                    ),
+                    Text('taxi'),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildTop() {
-    return Column(
-      children: [
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Icon(
-                  Icons.local_taxi,
-                  size: 40,
-                ),
-                Text('taxi'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Icon(
-                  Icons.local_taxi,
-                  size: 40,
-                ),
-                Text('taxi'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Icon(
-                  Icons.local_taxi,
-                  size: 40,
-                ),
-                Text('taxi'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Icon(
-                  Icons.local_taxi,
-                  size: 40,
-                ),
-                Text('taxi'),
-              ],
-            ),
-          ],
-        ),
-        const SizedBox(height: 20,),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                Icon(
-                  Icons.local_taxi,
-                  size: 40,
-                ),
-                Text('taxi'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Icon(
-                  Icons.local_taxi,
-                  size: 40,
-                ),
-                Text('taxi'),
-              ],
-            ),
-            InkWell(
-              onTap: () {
-                print('last taxi click..');
-              },
-              child: Column(
-                children: <Widget>[
-                  Icon(
-                    Icons.local_taxi,
-                    size: 40,
-                  ),
-                  Text('taxi'),
-                ],
-              ),
-            ),
-            Opacity(
-              opacity: 0.0,
-              child: Column(
-                children: <Widget>[
-                  Icon(
-                    Icons.local_taxi,
-                    size: 40,
-                  ),
-                  Text('taxi'),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
   Widget _buildMiddle() {
-    return const Text(
-      'middle'
+    const List<String> dummyItems = [
+      'https://cdn.pxabay.com/photo/2018/11/12/18/44/thanksgiving-3811492_1280.jpg',
+      'https://cdn.pxabay.com/photo/2018/11/12/18/44/thanksgiving-3811492_1280.jpg',
+      'https://cdn.pxabay.com/photo/2018/11/12/18/44/thanksgiving-3811492_1280.jpg',
+    ];
+
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 150,
+        autoPlay: true
+      ),
+      items: dummyItems.map((url) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  url,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+        );
+      }).toList(),
     );
   }
 
   Widget _buildBottom() {
-    return const Text(
-      'bottom'
-    );
+    return const Text('bottom');
   }
 }
