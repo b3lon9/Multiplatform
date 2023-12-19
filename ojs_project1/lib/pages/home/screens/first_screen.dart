c9990bcff6037a9782a3bdc10858f1744dc8847d
neander <b3xlon9@gmail.com>
2023-12-19 10:52:43 +0900

    ojs chapter01 - first screen 상단 부분 Icon나열 및 Row, Column, Padding 적용

--- ojs_project1/lib/pages/home/screens/first_screen.dart
+++ ojs_project1/lib/pages/home/screens/first_screen.dart
@@ -5,8 +5,125 @@ class FirstScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('first_screen'),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
+      child: Column(
+        children: <Widget>[
+          _buildTop(),
+          _buildMiddle(),
+          _buildBottom(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTop() {
+    return Column(
+      children: [
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('taxi'),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('taxi'),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('taxi'),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('taxi'),
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(height: 20,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('taxi'),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Icon(
+                  Icons.local_taxi,
+                  size: 40,
+                ),
+                Text('taxi'),
+              ],
+            ),
+            InkWell(
+              onTap: () {
+                print('last taxi click..');
+              },
+              child: Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('taxi'),
+                ],
+              ),
+            ),
+            Opacity(
+              opacity: 0.0,
+              child: Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.local_taxi,
+                    size: 40,
+                  ),
+                  Text('taxi'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMiddle() {
+    return const Text(
+      'middle'
+    );
+  }
+
+  Widget _buildBottom() {
+    return const Text(
+      'bottom'
     );
   }
 }
