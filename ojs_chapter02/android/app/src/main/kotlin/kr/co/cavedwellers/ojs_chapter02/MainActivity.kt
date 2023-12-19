f35378bb5fb7a43dcec890d0cce808ddaec366ec
neander <b3xlon9@gmail.com>
2023-12-19 12:11:28 +0900

    ojs chapter02 - first commit
    DefaultTabController ~ AppBar( tabs ~ Tab() ) ~ body: TabBarView( ) ~

---
+++ ojs_chapter02/android/app/src/main/kotlin/kr/co/cavedwellers/ojs_chapter02/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.co.cavedwellers.ojs_chapter02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ojs_chapter02/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.tag_faces)),
+              Tab(text: 'menu',),
+              Tab(icon: Icon(Icons.info), text: 'etc',)
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Container(color: Colors.yellow,),
+            Container(color: Colors.red,),
+            Container(color: Colors.blue,),
+          ],
+        ),
+      ),
+    );
+  }
+}
